7b420aa2598e289a977707a8168a917f7a2882cb
jihoon <ghoon.jang@webtoonscorp.com>
2022-12-05 00:43:19 +0900

    [dev/feature] 배포 준비

--- src/components/Output.js
+++ src/components/Output.js
@@ -6,10 +6,10 @@ const Output = ({ password, handleRefresh }) => {
         <label className="form-label">Generated password</label>
         <div className="row">
           <div className="col-10">
-            <input className="form-control" value={password} readOnly />
+            <input className="form-control mb-3" value={password} readOnly />
           </div>
           <div className="col-auto">
-          <button type="button" className="btn btn-primary" onClick={handleRefresh}>Refresh</button>
+            <button type="button" className="btn btn-primary" onClick={handleRefresh}>Refresh</button>
           </div>
         </div>
       </div>
